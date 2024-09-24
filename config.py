18a763944510924354e1040b14d38bb89b8f16f8
Kyler Dana <kymasterbox@gmail.com>
2024-09-24 14:58:09 -0700

    Removed unused

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://username:password@your-rds-endpoint/db_name')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://admin:DatabaseL0ck1849!@ky-database-1.cz86mwc2sye6.us-east-2.rds.amazonaws.com/')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
